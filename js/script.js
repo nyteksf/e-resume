8b22d9fda33582d31aa3e2270aaa2c2657105552
Joe <nyteksf@users.noreply.github.com>
2018-02-05 01:00:45 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function launchModal() {
 
     // When modal triggered, do this:  
     var emailAddress = decodeURIComponent("email%2Enytek%40gmail%2Ecom");   
-    alertify.confirm(emailAddress, function(e) {
+    alertify.confirm('Contact', emailAddress, function(e) {
         if (e) {
             alertify.success("Thank you. Your consideration is appreciated. <strong>[close]</strong>");
         }
@@ -27,7 +27,7 @@ let launchModalOnce = $("#email-link").mouseover(function (e) {
     if (launchModalOnce.true) return true;
     launchModalOnce.true = true;
     launchModal();
-	console.log("!!sdfsfsd!");
+	console.log("!!");
     $("span#email-link i.fa.fa-external-link").css("display", "none");
     $("button#alertify-ok").css("display","none");
     return false;
